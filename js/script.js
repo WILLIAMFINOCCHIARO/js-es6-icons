15608467fe199231b4b09371fa58df7f4d5c67d5
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-23 20:13:33 +0100

    almost

--- js/script.js
+++ js/script.js
@@ -8,3 +8,153 @@
 // Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina. Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi. Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
 // Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
 // N.B. :collisione: Il layout dovete farlo voi, ma potete personalizzarlo (modificato) 
+
+
+const boxElement = [
+	{
+		nameBox: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'danger'
+	},
+	{
+		nameBox: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'success'
+	},
+	{
+		nameBox: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'success'
+	},
+	{
+		nameBox: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'success'
+	},
+	{
+		nameBox: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'success'
+	},
+	{
+		nameBox: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'primary'
+	},
+	{
+		nameBox: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'primary'
+	},
+	{
+		nameBox: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'primary'
+	},
+	{
+		nameBox: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'primary'
+	}
+];
+
+console.log(boxElement);
+
+// Creare le classi dinamicamente
+
+for(i = 0; i < boxElement.length; i++) {
+  InsertClass(boxElement);
+
+};
+
+
+
+function InsertClass (member) {
+  const BoxLocal = document.getElementById("BoxLocal");
+  BoxLocal.innerHTML +=
+  
+  `
+  <div class=" col-2  ">
+    <div class="card" >
+      <div class="card-top fs-2 bg-${member[i].color} ">
+        <i class="${member[i].family} ${member[i].prefix}${member[i].nameBox}"></i>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${member[i].nameBox}</p>
+      </div>
+    </div>
+  </div>
+  
+  `
+
+};
+
+
+
